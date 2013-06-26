297dca56cefc30a8bbe366907a4c55c252f1cd28
Jeremy Shipman <jeremy@Jeremy-mbp.local>
2013-06-26 14:56:53 +1200

    NEW: allow setting css specifically

--- code/InlineStyledEmail.php
+++ code/InlineStyledEmail.php
@@ -2,9 +2,15 @@
 
 class InlineStyledEmail extends Email {
 
+	protected $css = null;
+
 	protected function parseVariables($isPlain = false) {
 		parent::parseVariables($isPlain);
-		$this->body = InlineStyler::convert($this->body);
+		$this->body = InlineStyler::convert($this->body,$this->css);
+	}
+
+	function setCSS($css){
+		$this->css = $css;
 	}
 
 }
--- code/InlineStyler.php
+++ code/InlineStyler.php
@@ -5,11 +5,12 @@
 */
 class InlineStyler{
 	
-	static function convert($htmlContent){
+	static function convert($htmlContent, $css = null)
+    {
+        $css = $css ? $css : self::getCSS($htmlContent);
 		require_once(ISEMAIL_PATH.'/thirdparty/CssToInlineStyles/CssToInlineStyles.php');
-		$cssToInlineStyles = new TijsVerkoyen\CssToInlineStyles\CssToInlineStyles(
-                $htmlContent, implode("\n\n", self::getCSS($htmlContent))
-            );
+		$cssToInlineStyles = new TijsVerkoyen\CssToInlineStyles\CssToInlineStyles($htmlContent, $css);
+        $cssToInlineStyles->setStripOriginalStyleTags();
 		return $cssToInlineStyles->convert();
 	}
 
@@ -21,7 +22,7 @@ class InlineStyler{
         // remove all control characters except CR, LF and tab
         $html = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/u', '', $html); // 00-09, 11-31, 127
         $dom->loadHTML($html);
-        return self::extractStylesheets($dom);
+        return implode("\n\n",self::extractStylesheets($dom));
 	}
 
 	/**
